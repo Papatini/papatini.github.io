69aac5ebabaccfef08c5f2d0c3256bf311809255
Papatini <apatinipj@gmail.com>
2026-01-29 18:59:26 +0000

    completed data shapes project

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -33,12 +33,24 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO BELOW HERE ////////
   /////////////////////////////////////////////////
-
+  // TODO 0 complete
   // TODO 1: create a new shape object and add it to the array
-  
-
+  var shape = {
+    color: "blue",
+    shape: "circle",
+    repeat: 3
+  }
+  dataShapes.push(shape)
   // TODO 2: add a new property to all data shapes
-  
+  for (var currentShape = 0; currentShape < dataShapes.length; currentShape++) {
+    if (dataShapes[currentShape].color === "red") {
+      dataShapes[currentShape].goodBehavior = "bounce"
+    } else if (dataShapes[currentShape].color === "blue") {
+      dataShapes[currentShape].goodBehavior = "blink"
+    } else {
+      dataShapes[currentShape].goodBehavior = "spin"
+    }
+  }
 
   // TODO 3-a: add a function that handles the static display type
   

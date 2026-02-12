da67a8befd3c973f9604d38779c3f2fc6132f6da
Papatini <apatinipj@gmail.com>
2026-02-12 18:16:35 +0000

    message

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -11,17 +11,43 @@ $(document).ready(function () {
   .css("left", left)
   .appendTo(elementID);
   }
-  function rollDie(dieId) {
+  function rollDie(dieID) {
+    $(dieID).empty();
     var randomNum = Math.ceil(Math.random() * 6);
-    
     console.log(randomNum);
+    if (randomNum === 1) {
+  makeDot(42.5, 42.5, dieID); // middle middle
+} else if (randomNum === 2) {
+  makeDot(17.5, 17.5, dieID); // top left
+  makeDot(67.5, 67.5, dieID); // bottom right
+} else if (randomNum === 3) {
+  makeDot(17.5, 17.5, dieID); // top left
+  makeDot(67.5, 67.5, dieID); // bottom right
+  makeDot(42.5, 42.5, dieID); // middle middle
+} else if (randomNum === 4) {
+  makeDot(67.5, 67.5, dieID); // bottom right
+  makeDot(17.5, 17.5, dieID); // top left
+  makeDot(17.5, 67.5, dieID); // bottom left
+  makeDot(67.5, 17.5, dieID); // top right
+} else if (randomNum === 5) {
+  makeDot(42.5, 42.5, dieID); // middle middle
+  makeDot(67.5, 67.5, dieID); // bottom right
+  makeDot(17.5, 17.5, dieID); // top left
+  makeDot(17.5, 67.5, dieID); // bottom left
+  makeDot(67.5, 17.5, dieID); // top right
+} else if (randomNum === 6) {
+  makeDot(42.5, 17.5, dieID); // middle left
+  makeDot(42.5, 67.5, dieID); // middle right
+  makeDot(67.5, 67.5, dieID); // bottom right
+  makeDot(17.5, 17.5, dieID); // top left
+  makeDot(17.5, 67.5, dieID); // bottom left
+  makeDot(67.5, 17.5, dieID); // top right
+}
     
   }
   function handleClick() {
     rollDie("#die")
   }
   $("#die").on("click", handleClick);
-  makeDot(50, 50, "#die"); // middle middle
-makeDot(25, 25, "#die"); // top left
-makeDot(75, 75, "#die"); // bottom right
 });
+

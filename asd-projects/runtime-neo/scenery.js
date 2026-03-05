0b00c30be272fb99b8e61b705504a03ac7e8f57a
Papatini <apatinipj@gmail.com>
2026-03-05 18:54:55 +0000

    completed runtime-neo project

--- asd-projects/runtime-neo/scenery.js
+++ asd-projects/runtime-neo/scenery.js
@@ -14,11 +14,11 @@ const scenery = {
   building: {
     imageUrl: "images/backgrounds/building.png",
     loopWidth: 1400,
-    instances: [{ x: 400, width: 100, height: 300, speedX: -2 }],
+    instances: [{x:0, width: 100, height: 300, speedX: -3 }, {x:200, width: 90, height: 250, speedX: -3 }, { x: 400, width: 100, height: 300, speedX: -3 }, {x:600, width: 110, height: 400, speedX: -3 }, {x:800, width: 100, height: 350, speedX: -3 }, {x:1000, width: 95, height: 200, speedX: -3 }, {x:1200, width: 110, height: 400, speedX: -3 },  ],
   },
   lamp: {
     imageUrl: "images/backgrounds/lamp.png",
     loopWidth: 1400,
-    instances: [{ x: 700, width: 50, height: 150, speedX: 0 }],
+    instances: [{ x: 100, width: 65, height: 165, speedX: -3 }, { x:300, width: 35, height: 135, speedX: -3 }, { x: 500, width: 60, height: 160, speedX: -3 }, { x: 700, width: 50, height: 150, speedX: -3 }, { x: 900, width: 40, height: 140, speedX: -3 }, { x: 1100, width: 60, height: 160, speedX: -3 }, { x: 1300, width: 50, height: 150, speedX: -3 }],
   },
 };
--- asd-projects/runtime-neo/student.js
+++ asd-projects/runtime-neo/student.js
@@ -1,21 +1,64 @@
 function moveScenery() {
   // TODO 2: Move background scenery based on current level speed
+ 
+  for (var i = 0; i < scenery.building.instances.length; i++) {
+    var buildingInstance = scenery.building.instances[i];
+    buildingInstance.x = buildingInstance.x + buildingInstance.speedX + currentLevel.speed
+    if (buildingInstance.x + buildingInstance.width < 0 ) {
+      buildingInstance.x = scenery.building.loopWidth;
+    }
+  }
+  for (var i = 0; i < scenery.lamp.instances.length; i++) {
+    var lampInstance = scenery.lamp.instances[i];
+    lampInstance.x = lampInstance.x + lampInstance.speedX + currentLevel.speed
+    if (lampInstance.x + lampInstance.width < 0 ) {
+      lampInstance.x = scenery.lamp.loopWidth
+    }
+  }
 }
 
 function generateLevel() {
   // TODO 3: Generate the current level's game objects
+  for (var i = 0; i < currentLevel.gameObjects.length; i++) {
+    var currentObject = currentLevel.gameObjects[i];
+    create(currentObject)
+  }
 }
 
 function create(obj) {
   // TODO 4: Create a game object based on its type and kind
+  if (obj.type === "obstacle") {
+    makeObstacle(obj)
+  } if (obj.type === "enemy") {
+    makeEnemy(obj)
+  } if (obj.type === "powerup") {
+    makePowerup(obj)
+  } if (obj.type === "goal") {
+    makeGoal(obj)
+  } if (obj.type === "platform") {
+    makePlatform(obj)
+  }
 }
 
 function filterObjects(type) {
   // TODO 5: Return only the game objects of the specified type
+  var matches = []
+  for (var i = 0; i < gameObjects.length; i++) {
+    if (gameObjects[i].type === type) {
+      matches.push(i)
+    }
+  }
+  return matches
+  
 }
 
 function moveGameObjects(objectList) {
   // TODO 6: Move all game objects of a single type based on speeds
+  for (var i = 0; i < objectList.length; i++) {
+    var currentObject2 = objectList[i]
+    currentObject2.x = currentObject2.x + currentObject2.speedX
+    currentObject2.x = currentObject2.x - currentLevel.speed
+  }
 }
 
 function handleProjectileCollisions() {
